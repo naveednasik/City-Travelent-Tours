e1369a2efca78757fcacefc6dbea8714bc1ae194
Nasik Naveed <53467503+naveednasik@users.noreply.github.com>
2025-01-17 16:32:32 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
-// import logo from './logo.svg';
 import './App.css';
-// import Navbar from './components/Navbar/Navbar';
 import TravelentTourPage from './page/TravelentTourPage/TravelentTourPage';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
